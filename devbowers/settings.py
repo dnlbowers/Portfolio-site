e0b86bcb31f57954b88093688c68ae1c56c67028
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-06-28 23:59:48 +0200

    tweak for initial deploy

--- devbowers/settings.py
+++ devbowers/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'home',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
