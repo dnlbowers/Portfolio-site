9cdbbadbc7b92f3dec363e0fc4fcb483293e182a
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-06-30 02:10:30 +0200

    tidy css, make sure footer is at bottom of the page and responsive

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# Create your views here.
+
 class HomePage(TemplateView):
     """"
     View to return index page
     """
     template_name = 'home/index.html'
-    
