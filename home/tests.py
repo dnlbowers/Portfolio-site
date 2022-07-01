d20effdc3d69a5d27ef07a53d92bd396b0ceba1f
David Bowers <dnlbowers@gmail.com>
2022-07-01 15:03:46 +0200

    write test for homepage

--- home/tests.py
+++ home/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class TestPage(TestCase):
     
 
-   def test_index_page(self):
+    def test_index_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home/index.html")
