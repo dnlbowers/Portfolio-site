40b5ee8d79c55573d5f2a6fa657aa8f35dcbfe05
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-07-02 20:53:39 +0200

    set up template for portfolio page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from .models import Project
 
 
 class PortfolioPage(TemplateView):
@@ -6,3 +7,8 @@ class PortfolioPage(TemplateView):
     View to return index page
     """
     template_name = 'portfolio/portfolio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.all()
+        return context
